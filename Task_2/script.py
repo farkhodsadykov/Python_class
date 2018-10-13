66897e75262fb05f7320ecbb6f0c0bb99e9e3925
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-10-13 00:17:41 -0500

    Created instroctions for students

--- Task_2/script.py
+++ Task_2/script.py
@@ -1,9 +1,11 @@
 import requests
 import json
 
+# list of cities 
 newlist = ['Chicago', 'Denver', 'San Jose']
 
 
+# Runing the for loop for each cities
 for address in newlist:
     data = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=' + address  +',NY&key=token')
     data = data.json()
